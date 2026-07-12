38331080b505323f77e7c07d7392108f7e4f6b0f
abobo1401-farm <abobo1401@gmail.com>
2026-07-12 14:28:04 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Console で取得した設定に置き換えてください。
+// 使い方は README.md に書いてあります。
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
